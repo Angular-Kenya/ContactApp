51a203b0015c84da2ac4f70cd504a5dccfcbe0ac
novak254 <ndemokelvinonkundi@gmail.com>
2020-04-21 03:02:07 +0300

    move typescript to dependencies

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,38 +1,37 @@
-import mongoose,{model} from "mongoose"
-import { IUserModel, IUser } from "../interfaces"
+import mongoose, { model } from "mongoose";
+import { IUserModel, IUser } from "../interfaces";
 
 const UserSchema = new mongoose.Schema({
-    first_name:{
-        type:String,
-        required:true,
-        max:255,
-        min:3
-    },
-    last_name:{
-        type:String,
-        required:true,
-        max:255,
-        min:3
-    },
-    phone_number:{
-        type:String,
-        required:true,
-        max:255,
-        min:10
-    },
-    location:{
-        type:String,
-        required:true,
-        max:255,
-        min:3
-    },
-    email:{
-        type:String,
-        required:true,
-        max:255,
-        min:3
-    },
+  first_name: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 3,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 3,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 10,
+  },
+  location: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 3,
+  },
+});
 
-})
-
-export const User:IUserModel = model<IUser, IUserModel>('user', UserSchema)
+export const User: any = model<any>("user", UserSchema);
